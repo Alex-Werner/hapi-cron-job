ec9760f0be700e91378562c4b9a666648d9f5398
Alex WERNER <alex@werner.fr>
2016-10-12 21:48:23 +0200

    [Fix] Parser day/days and week/weeks

--- index.js
+++ index.js
@@ -40,9 +40,9 @@ const _parseText = function (string) {
         //
         second: /^(s|sec(ond)?(s)?)\b/,
         minute: /^(m|min(ute)?(s)?)\b/,
-        hour: /^(h|hour(s)?)\b/,
-        day: /^(d|ay(s)?)\b/,
-        week: /^(w|eek(s)?)\b/,
+        hour: /^(h(our)?(s)?)\b/,
+        day: /^(d(ay)?(s)?)\b/,
+        week: /^(w(eek)?(s)?)\b/,
         //
         clockTime: /^(([0]?[1-9]|1[0-2]):[0-5]\d(\s)?(am|pm))\b/,
         time: /^(([0]?\d|1\d|2[0-3]):[0-5]\d)\b/
